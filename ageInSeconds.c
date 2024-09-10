cded9732977d5b3758aa882a7dfb6d3f7ad9124f
MithleshKr-Pro <coding4u2@gmail.com>
2024-09-10 20:22:27 +0530

    reUpdated

---
+++ ageInSeconds.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float age_Sec,age_Year;
+    printf("Enter your age in Years = ");
+    scanf("%f",&age_Year);
+    age_Sec=age_Year*3.156E7;
+    printf("Your Age in Seconds = %f",age_Sec);
+    return 0;
+}
