04348139d372eee1b7a8e13fa974f595f3d21baf
MithleshKr-Pro <coding4u2@gmail.com>
2024-09-11 02:57:15 +0530

    reUpdated

--- maturityAmount.c
+++ maturityAmount.c
@@ -2,7 +2,9 @@
 #include <math.h>
 int main(){
     float p,t,r=5,i,mat_Amount;
+    printf("Principal Amount = ");
     scanf("%f",&p);
+    printf("Time = ");
     scanf("%f",&t);
     i=(p*r*t)/100;
     mat_Amount=i+p;
