d71d0ca57046920e2d48795a04cc08ff2c9dab7f
MithleshKr-Pro <coding4u2@gmail.com>
2024-09-11 02:56:05 +0530

    reUpdated

--- velKmToM.c
+++ velKmToM.c
@@ -2,9 +2,8 @@
 int main(){
     float vel_Km,vel_M;
     printf("Enter Velocity (Km/h) = ");
-    scanf("%f ",&vel_Km);
+    scanf("%f",&vel_Km);
     vel_M=vel_Km*5/18;
     printf("Velocity (m/sec) = %f m/sec",vel_M);
     return 0;
-
 }
