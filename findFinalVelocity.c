d6c7667ca71288c449d07f1b19bb081939f4b9a9
MithleshKr-Pro <coding4u2@gmail.com>
2024-09-10 20:06:00 +0530

    reUpdated

--- findFinalVelocity.c
+++ findFinalVelocity.c
@@ -2,5 +2,13 @@
 #include <math.h>
 int main(){
     float u,v,a,t;
-    
+    printf("Enter initial Velocity = ");
+    scanf("%f",&u);
+    printf("Acceleration = ");
+    scanf("%f",&a);
+    printf("Time taken = ");
+    scanf("%f",&t);
+    v=u+a*t;
+    printf("Final Velocity = %f",v);
+    return 0;
 }
