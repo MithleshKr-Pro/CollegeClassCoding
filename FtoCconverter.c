2f1f995fa613e440c2b6abbf44a792cdc3e1f348
MithleshKr-Pro <coding4u2@gmail.com>
2024-09-11 02:53:13 +0530

    reUpdated

--- FtoCconverter.c
+++ FtoCconverter.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    float f,c=37;
-    f=(9*c/5)+32;
-    printf("Celsius to Farenheit = %f",f);
+    float c,f=98;
+    c=(f-32)*5/9;
+    printf("Farenheit to Celsius = %f",c);
     return 0;
 }
